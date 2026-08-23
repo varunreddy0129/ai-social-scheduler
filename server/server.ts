76d5d78d1546882fc2e94ec69b24b3c5406edf94
Varun Reddy <varunreddy0129@gmail.com>
2026-08-23 22:09:16 +0530

    app.listen changed

--- server/server.ts
+++ server/server.ts
@@ -25,7 +25,7 @@ await connectDB();
 app.use(cors())
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
 app.get('/', (_req: Request, res: Response) => {
     res.send('Server is Live!');
@@ -47,6 +47,6 @@ app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     res.status(500).send('Something went wrong!');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on port ${port}`);
 });
